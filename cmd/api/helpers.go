646fad8a686f5d7f602c8639fdadaf5cc387f735
Faisal Abid <ifaisalabid1@gmail.com>
2026-01-19 18:50:57 +0530

    Send json response through show movie handler, create centralized error helper

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"maps"
 	"net/http"
 	"strconv"
 
@@ -17,4 +19,24 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+type envelope map[string]any
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	maps.Copy(w.Header(), headers)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
